430798a164efb9a1387aec4b53615631ad850ec7
bashir-hasanov <beshir.hasanov@gmail.com>
2022-09-10 11:39:35 -0400

    Built and completed project, started working with beans

--- src/main/java/com/cydeo/BankSimulationAppApplication.java
+++ src/main/java/com/cydeo/BankSimulationAppApplication.java
@@ -1,13 +1,23 @@
 package com.cydeo;
 
+import com.cydeo.repository.TransactionRepository;
+import com.cydeo.service.AccountService;
+import com.cydeo.service.TransactionService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class BankSimulationAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BankSimulationAppApplication.class, args);
+
+      ApplicationContext container = SpringApplication.run(BankSimulationAppApplication.class, args);
+
+      // get account and transaction service beans
+        AccountService accountService = container.getBean(AccountService.class);
+        TransactionService transactionService = container.getBean(TransactionService.class);
+
     }
 
 }
--- src/main/java/com/cydeo/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TransactionServiceImpl.java
@@ -5,20 +5,28 @@ import com.cydeo.enums.AccountType;
 import com.cydeo.exception.AccountOwnershipException;
 import com.cydeo.exception.BadRequestException;
 import com.cydeo.exception.BalanceNotSufficientException;
+import com.cydeo.exception.UnderConstructionException;
 import com.cydeo.model.Account;
 import com.cydeo.model.Transaction;
 import com.cydeo.repository.AccountRepository;
+import com.cydeo.repository.TransactionRepository;
 import com.cydeo.service.TransactionService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+@Component
 public class TransactionServiceImpl implements TransactionService {
 
-    AccountRepository accountRepository;
+    @Value("${under_construction}")
+    private boolean underConstruction;
 
+    AccountRepository accountRepository;
+    TransactionRepository transactionRepository;
     public TransactionServiceImpl(AccountRepository accountRepository) {
         this.accountRepository = accountRepository;
     }
@@ -27,15 +35,26 @@ public class TransactionServiceImpl implements TransactionService {
     public Transaction makeTransfer(Account sender, Account receiver,
                                     BigDecimal amount, Date creationDate, String message) {
 
-        validateAccount(sender, receiver);
-        checkAccountOwnership(sender, receiver);
-        executeBalanceAndUpdateIfRequired(amount, sender, receiver);
+        if (!underConstruction) {
+            validateAccount(sender, receiver);
+            checkAccountOwnership(sender, receiver);
+            executeBalanceAndUpdateIfRequired(amount, sender, receiver);
         /*
         after all validations completed, and money is transferred
         we need to create Transaction object and save/return it
         Please create needed classes/methods for this step, save the transactions
          */
-        return null;
+            Transaction transaction = Transaction.builder().amount(amount)
+                    .sender(sender.getId()).receiver(receiver.getId())
+                    .creationDate(creationDate).message(message).build();
+
+            return transactionRepository.save(transaction);
+
+        } else {
+
+            throw new UnderConstructionException("App is under construction, try again later.");
+        }
+
     }
 
     private void executeBalanceAndUpdateIfRequired(BigDecimal amount, Account sender, Account receiver) {
@@ -94,6 +113,6 @@ public class TransactionServiceImpl implements TransactionService {
 
     @Override
     public List<Transaction> findAllTransactions() {
-        return null;
+        return transactionRepository.findAll();
     }
 }
