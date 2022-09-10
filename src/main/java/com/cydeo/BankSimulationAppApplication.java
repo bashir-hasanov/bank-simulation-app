e9cb69dbc908d307d36086bf081fb19d759b2a09
bashir-hasanov <beshir.hasanov@gmail.com>
2022-09-10 12:02:51 -0400

    Practice

--- src/main/java/com/cydeo/BankSimulationAppApplication.java
+++ src/main/java/com/cydeo/BankSimulationAppApplication.java
@@ -1,11 +1,16 @@
 package com.cydeo;
 
+import com.cydeo.enums.AccountType;
+import com.cydeo.model.Account;
 import com.cydeo.repository.TransactionRepository;
 import com.cydeo.service.AccountService;
 import com.cydeo.service.TransactionService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 @SpringBootApplication
 public class BankSimulationAppApplication {
@@ -18,6 +23,20 @@ public class BankSimulationAppApplication {
         AccountService accountService = container.getBean(AccountService.class);
         TransactionService transactionService = container.getBean(TransactionService.class);
 
+        // create 2 accounts: sender & receiver
+        Account sender = accountService.createNewAccount(BigDecimal.valueOf(70), new Date(),
+                AccountType.CHECKING, 1L);
+        Account receiver = accountService.createNewAccount(BigDecimal.valueOf(50), new Date(),
+                AccountType.SAVING, 1L);
+        Account receiver2 = null;
+
+        accountService.listAllAccount().forEach(System.out :: println);
+
+        transactionService.makeTransfer(sender,receiver,new BigDecimal(40),new Date(),"Transaction 1");
+
+        System.out.println(transactionService.findAllTransactions().get(0));
+
+        accountService.listAllAccount().forEach(System.out::println);
     }
 
 }
